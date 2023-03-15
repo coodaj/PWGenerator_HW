0cc71e2629472ebed3f470aad040591622189fd5
Dana Cooper <coodaj@gmail.com>
2023-03-14 21:53:57 -0600

    updated file tree

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword(length) {
+  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+}
+
+generateButton.addEventListener('click', function() {
+  // Code to execute when the button is clicked
+});
+
+generateBtn.addEventListener("click", writePassword);
